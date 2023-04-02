b733468a99c3c37ccc6699b0ca99beef30eb6cfc
codingpy <1658046076@qq.com>
2023-04-02 16:44:10 +0800

    initial project version

---
+++ official_accounts.py
@@ -0,0 +1,92 @@
+import re
+import hashlib
+import base64
+
+import xmltodict
+from Crypto.Util.Padding import pad, unpad
+from Crypto.Cipher import AES
+from Crypto.Random import get_random_bytes
+
+
+def parse(xml):
+    return xmltodict.parse(xml, postprocessor=postprocessor)['xml']
+
+
+def unparse(dic):
+    return xmltodict.unparse(
+        {'xml': dic}, full_document=False, preprocessor=preprocessor
+    )
+
+
+def postprocessor(path, key, value):
+    key = to_snake(key)
+
+    if key in ['create_time', 'msg_id', 'scale']:
+        value = int(value)
+
+    if key in ['location__x', 'location__y']:
+        value = float(value)
+
+    return key, value
+
+
+def preprocessor(key, value):
+    if key != 'xml':
+        key = to_camel(key)
+
+    return key, value
+
+
+def to_snake(s):
+    return re.sub('(?<!^)(?=[A-Z])', '_', s).lower()
+
+
+def to_camel(s):
+    return re.sub('_(?=[A-Z])', '', s.title())
+
+
+def verify(signature, *args):
+    return signature == sign(*args)
+
+
+def sign(*args):
+    return sha1(''.join(sorted(args)))
+
+
+def sha1(s):
+    return hashlib.sha1(s.encode()).hexdigest()
+
+
+def encrypt(pt, encoding_aes_key):
+    cipher = get_cipher(encoding_aes_key)
+
+    return base64.b64encode(cipher.encrypt(pad(pt, 32)))
+
+
+def decrypt(ct, encoding_aes_key):
+    cipher = get_cipher(encoding_aes_key)
+
+    return unpad(cipher.decrypt(base64.b64decode(ct)), 32)
+
+
+def get_cipher(encoding_aes_key):
+    key = base64.b64decode(encoding_aes_key + '=')
+
+    return AES.new(key, AES.MODE_CBC, iv=key[:AES.block_size])
+
+
+def pack(msg, app_id):
+    msg_len = len(msg)
+
+    return (
+        get_random_bytes(16)
+        + msg_len.to_bytes(4, byteorder='big')
+        + msg
+        + app_id
+    )
+
+
+def unpack(buffer):
+    msg_len = int.from_bytes(buffer[16:20], byteorder='big')
+
+    return buffer[20 : 20 + msg_len], buffer[20 + msg_len :]
