a3342cfc351c82be324f8aa07c2ec8360aca4d11
codingpy <1658046076@qq.com>
2024-07-09 14:37:24 +0000

    feat: support plaintext mode

--- official_accounts/views.py
+++ official_accounts/views.py
@@ -14,35 +14,36 @@ def on_msg(f):
         if isinstance(req, AbstractView):
             req = req.request
 
-        if req.query["encrypt_type"] != "aes":
+        if not verify(req):
             raise web.HTTPBadRequest
 
-        m = utils.parse_xml(await req.text())
+        secure_mode = req.query.get("encrypt_type") == "aes"
 
-        if not auth.verify(
-            req.query["msg_signature"],
-            req.app["token"],
-            req.query["timestamp"],
-            req.query["nonce"],
-            m["encrypt"],
-        ):
-            raise web.HTTPBadRequest
+        d = utils.parse_xml(await req.text())
+        if secure_mode:
+            if not auth.verify(
+                req.query["msg_signature"],
+                req.app["token"],
+                req.query["timestamp"],
+                req.query["nonce"],
+                d["encrypt"],
+            ):
+                raise web.HTTPBadRequest
 
-        m = msgs.create_msg(
-            utils.parse_xml(
+            d = utils.parse_xml(
                 auth.decrypt_msg(
-                    m["encrypt"], req.app["app_id"], req.app["encoding_aes_key"]
+                    d["encrypt"], req.app["app_id"], req.app["encoding_aes_key"]
                 )
             )
-        )
+
+        m = msgs.create_msg(d)
 
         req["msg"] = m
-
         res = await f(*args, **kwds)
 
         # If the server cannot process and reply to the message within five seconds,
         # it can return an empty string.
-        if not res:
+        if res is None:
             return web.Response(text="")
 
         if isinstance(res, str):
@@ -54,16 +55,16 @@ def on_msg(f):
             "create_time": m.create_time,
             **res,
         }
+        msg = utils.to_xml(res)
 
-        msg_encrypt = auth.encrypt_msg(
-            utils.to_xml(res), req.app["app_id"], req.app["encoding_aes_key"]
-        )
-        msg_signature = auth.sign(
-            req.app["token"], res["create_time"], req.query["nonce"], msg_encrypt
-        )
-
-        return web.Response(
-            text=utils.to_xml(
+        if secure_mode:
+            msg_encrypt = auth.encrypt_msg(
+                msg, req.app["app_id"], req.app["encoding_aes_key"]
+            )
+            msg_signature = auth.sign(
+                req.app["token"], res["create_time"], req.query["nonce"], msg_encrypt
+            )
+            msg = utils.to_xml(
                 {
                     "encrypt": msg_encrypt,
                     "msg_signature": msg_signature,
@@ -71,19 +72,24 @@ def on_msg(f):
                     "nonce": req.query["nonce"],
                 }
             )
-        )
+
+        return web.Response(text=msg)
 
     return wrapper
 
 
+def verify(request):
+    return auth.verify(
+        request.query["signature"],
+        request.app["token"],
+        request.query["timestamp"],
+        request.query["nonce"],
+    )
+
+
 class Handle(web.View):
     async def get(self):
-        if auth.verify(
-            self.request.query["signature"],
-            self.request.app["token"],
-            self.request.query["timestamp"],
-            self.request.query["nonce"],
-        ):
+        if verify(self.request):
             return web.Response(text=self.request.query["echostr"])
 
     @on_msg
